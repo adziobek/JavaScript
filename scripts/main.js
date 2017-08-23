855b4a88648e63dd7ef45b69c5593bfab6582a94
adziobek <adziobek@apreel.com>
2017-08-23 17:46:29 +0200

    Change src attribute onclick

--- scripts/main.js
+++ scripts/main.js
@@ -1,4 +1,16 @@
 window.onload = function () {
     var myHeading = document.querySelector('h1');
     myHeading.textContent = "Hello world!";
+
+//change attribute event
+    var myImage = document.querySelector('img');
+
+    myImage.onclick = function () {
+        var mySrc = myImage.getAttribute('src');
+        if (mySrc === 'images/firefox-icon.png') {
+            myImage.setAttribute('src', 'images/chrome-icon.png');
+        } else {
+            myImage.setAttribute('src', 'images/firefox-icon.png')
+        }
+    };
 };
