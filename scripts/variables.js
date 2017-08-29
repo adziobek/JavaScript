fafcf734646d98ffc33e3fdaefd5657537fd720d
adziobek <adziobek@apreel.com>
2017-08-29 10:05:03 +0200

    Numbers basics

--- scripts/variables.js
+++ scripts/variables.js
@@ -8,3 +8,12 @@ function variableHoisting() {
     var bar = 11;
     console.log(bar);
 }
+
+//Number
+var myAge = 24;
+//String
+var myName = 'Andrzej';
+//Array
+var myUncles = ['Tom', 'Peter', 'Lucas'];
+//Object
+var myDog = {name: 'Azor', breed: 'Pitbull'};
