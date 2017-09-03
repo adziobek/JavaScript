405f0fb2794c25b0dd0ff574691037ae3297dd4b
adziobek <adziobek@apreel.com>
2017-09-03 19:27:29 +0200

    arrays: length, slice, indexof

---
+++ scripts/arrays.js
@@ -0,0 +1,8 @@
+console.log('***arrays***');
+//array of different type object
+var random = ['Andrzej', 3, 5, 6];
+console.log(random);
+for (var i = 0; i < random.length; i++) {
+    console.log(typeof random[i]);
+}
+console.log('***arrays***');
